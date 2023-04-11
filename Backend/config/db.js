0c6cdc6d899da76a09beefbf0e91a5d2d7d263a6
Aditya Singh Ghosh <89689829+Adii1707@users.noreply.github.com>
2023-04-11 15:24:52 +0530

    Delete Backend directory

--- Backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
-require("dotenv").config();
-
-const connection = mongoose.connect(process.env.URL);
-
-module.exports = {connection}
